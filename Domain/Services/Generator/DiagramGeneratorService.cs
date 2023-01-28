ccf6d5d71b7e2a52a26a6f71aad9c0c4349aab03
Eduardo Rauchbach <eduardo.rauchbach@gmail.com>
2023-01-28 13:54:35 -0300

    Diagram and doc

--- Domain/Services/Generator/DiagramGeneratorService.cs
+++ Domain/Services/Generator/DiagramGeneratorService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using WorkUtilities.Domain.Models;
@@ -23,89 +24,181 @@ namespace WorkUtilities.Domain.Services.Generator
 
         public string ParseFromGenerator(GeneratorModel model)
         {
-            List<EntryModel> entryModels = new List<EntryModel>();
+            var multigraph = TopologicalSort(model.EntryModels, 6);
 
-            diagram.AppendLine("```mermaid");
-            diagram.AppendLine("%%{ init: { 'classDiagram': { 'useMaxWidth': false }}}%%");
-            diagram.AppendLine("classDiagram");
-            //diagram.AppendLine("graph LR");
-
-            foreach (EntryModel e in TopologicalSort(model.EntryModels))
+            foreach (var graphInfo in multigraph)
             {
-                e.PreProcess();
-                ParseFromEntry(diagram, model, e);
-            }
+                diagram.AppendLine("```mermaid");
+                //diagram.AppendLine("classDiagram");
+                diagram.AppendLine("graph TB");
+                diagram.AppendLine("%%{ init: { 'theme': 'neutral', 'flowchart': { 'useMaxWidth': false, 'curve': 'basis' }}}%%");
 
-            foreach (EntryModel e in model.EntryModels)
-            {
-                BuildRelations(diagram, 1, model, e);
-            }
+                foreach (EntryModel e in graphInfo)
+                {
+                    e.PreProcess();
+                    ParseFromEntry(diagram, model, e);
+                }
 
-            diagram.AppendLine("```");
-            diagram.AppendLine();
+                foreach (EntryModel e in graphInfo)
+                {
+                    BuildRelations(diagram, 1, model, e);
+                }
+
+                diagram.AppendLine("```");
+                diagram.AppendLine();
+            }
             diagram.AppendLine("<br/><br/><br/>");
             diagram.Append(entityList);
 
             return diagram.ToString();
 
-            static List<EntryModel> TopologicalSort(List<EntryModel> objects)
+
+        }
+
+        private List<List<EntryModel>> TopologicalSort(List<EntryModel> entries, int maxLimit)
+        {
+            var sortedEntries = new List<EntryModel>();
+            var visited = new HashSet<string>();
+            var groups = new List<List<EntryModel>>();
+            var group = new List<EntryModel>();
+
+            // Helper function to perform topological sort
+            void DFS(EntryModel entry)
             {
-                // Create a dictionary to store in-degree of each object
-                Dictionary<string, int> inDegree = new Dictionary<string, int>();
-                foreach (EntryModel obj in objects)
+                visited.Add(entry.Name);
+                foreach (var child in entries.Where(x => x.Relationships.Any(y => y.TargetName == entry.Name)))
                 {
-                    inDegree[obj.Name] = 0;
+                    DFS(child);
                 }
+                sortedEntries.Add(entry);
+            }
 
-                // Count in-degree of each object
-                foreach (EntryModel obj in objects)
+            // Perform topological sort
+            foreach (var entry in entries)
+            {
+                if (!visited.Contains(entry.Name))
                 {
-                    foreach (var child in obj.Relationships)
-                    {
-                        inDegree[child.TargetName]++;
-                    }
+                    DFS(entry);
                 }
+            }
 
-                // Create a queue to store objects with in-degree 0
-                Queue<EntryModel> queue = new Queue<EntryModel>();
-                foreach (EntryModel obj in objects)
+            // Iterate through sorted entries and form groups
+            for (int i = sortedEntries.Count - 1; i >= 0; i--)
+            {
+                var current = sortedEntries[i];
+
+                // Check if current entry has a parent in current group
+                if (group.Any(x => x.Relationships.Any(y => y.TargetName == current.Name)))
                 {
-                    if (inDegree[obj.Name] == 0)
-                    {
-                        queue.Enqueue(obj);
-                    }
+                    group.Add(current);
                 }
-
-                // Create a list to store the sorted objects
-                List<EntryModel> sortedObjects = new List<EntryModel>();
-
-                // Perform topological sort
-                while (queue.Count > 0)
+                else
                 {
-                    var obj = queue.Dequeue();
-                    sortedObjects.Add(obj);
+                    // Check if current entry has a child in any previous groups
+                    var hasChildInPreviousGroup = groups.Any(g => g.Any(x => x.Relationships.Any(y => y.TargetName == current.Name)));
 
-                    foreach (EntryModel currentObject in objects)
+                    if (hasChildInPreviousGroup)
                     {
-                        if (currentObject.Name != obj.Name)
-                            continue;
-                        foreach (var child in currentObject.Relationships)
+                        if (!group.Any(x => x.NameDB == current.NameDB))
                         {
-                            inDegree[child.TargetName]--;
-                            if (inDegree[child.TargetName] == 0)
+                            group.Add(current);
+                        }
+                    }
+                    else
+                    {
+                        // Check if group has reached max limit
+                        if (group.Count >= maxLimit)
+                        {
+                            // Add current entry to new group
+                            group = new List<EntryModel> { current };
+                            groups.Add(group);
+                        }
+                        else
+                        {
+                            if (!group.Any(x => x.NameDB == current.NameDB))
                             {
-                                queue.Enqueue(objects.First(x => x.Name == child.TargetName));
+                                group.Add(current);
                             }
                         }
                     }
                 }
+            }
 
-                sortedObjects.Reverse();
-                return sortedObjects;
+            // Add final group to list of groups
+            groups.Add(group);
+            MergeListsWithRecurringEntries(groups);
+
+            return SplitClear(groups).Where(x => x.Any()).ToList();
+
+            void MergeListsWithRecurringEntries(List<List<EntryModel>> listOfLists)
+            {
+                bool executed = false;
+                for (int i = 0; i < listOfLists.Count; i++)
+                {
+                    for (int j = i + 1; j < listOfLists.Count; j++)
+                    {
+                        var recurringEntries = listOfLists[i].Intersect(listOfLists[j]).ToList();
+                        if (recurringEntries.Any())
+                        {
+                            executed = true;
+
+                            listOfLists[i].AddRange(listOfLists[j]);
+                            listOfLists[i] = listOfLists[i].Distinct().ToList();
+                            listOfLists.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                }
+                if (executed)
+                {
+                    listOfLists.ForEach(x =>
+                    {
+                        x = x.DistinctBy(x => x.NameDB).ToList();
+                        x.ForEach(y =>
+                        {
+                            y.Relationships = y.Relationships.DistinctBy(r => r.TargetName).ToList();
+                        });
+                    });
+                    MergeListsWithRecurringEntries(listOfLists);
+                }
+            }
+
+            List<List<EntryModel>> SplitClear(List<List<EntryModel>> listOfLists)
+            {
+                var listAgg = new List<List<EntryModel>>();
+                var agg = new List<EntryModel>();
+
+                for (int i = listOfLists.Count - 1; i >= 0; i--)
+                {
+                    var exiting = listOfLists[i].Where(x => (x.Relationships?.Count == 0) && !listOfLists[i].Any(y => y.Relationships.Any(z => z.TargetName == x.Name)));
+                    if (exiting.Any())
+                    {
+                        foreach (var item in exiting)
+                        {
+                            if (agg.Count <= maxLimit)
+                            {
+                                if (!agg.Any(x => x.NameDB == item.NameDB))
+                                {
+                                    agg.Add(item);
+                                }
+                            }
+                            else
+                            {
+                                listAgg.Add(agg);
+                                agg = new List<EntryModel>();
+                            }
+                        }
+                        listOfLists[i].RemoveAll(x => exiting.Any(y => y.Name == x.Name));
+                    }
+                }
+                if (agg.Any())
+                {
+                    listAgg.Add(agg);
+                }
+
+                return listOfLists.Concat(listAgg).ToList();
             }
         }
-
-
 
         public void ParseFromEntry(StringBuilder builder, GeneratorModel model, EntryModel entry)
         {
@@ -115,8 +208,8 @@ namespace WorkUtilities.Domain.Services.Generator
             {
                 tab = 1;
 
-                diagram.AppendCode(tab, "class " + entry.NameDB + "{", 1);
-                //diagram.AppendCode(tab, $"{entry.NameDB}[\"{entry.NameDB}<br/>", 1);
+                //diagram.AppendCode(tab, "class " + entry.NameDB + "{", 1);
+                diagram.AppendCode(tab, $"{entry.NameDB}[\"{entry.NameDB}", 0);
                 tab++;
 
                 entityList.AppendCode(0, $"## {entry.NameDB}", 2);
@@ -128,8 +221,8 @@ namespace WorkUtilities.Domain.Services.Generator
                 entityList.AppendLine();
 
                 tab--;
-                diagram.AppendCode(tab, "}", 2);
-                //diagram.AppendCode(tab, "\"]", 2);
+                //diagram.AppendCode(tab, "}", 2);
+                diagram.AppendCode(0, "\"]", 2);
             }
             catch
             {
@@ -145,7 +238,7 @@ namespace WorkUtilities.Domain.Services.Generator
                 var key = p.IsKey ? CheckYes : CheckNo;
                 var auto = p.IsAutoGenerated ? CheckYes : CheckNo;
 
-                result.AppendCode(tab, $"+{p.TypeDB} {p.NameDB}", 1);
+                //result.AppendCode(tab, $"+{p.TypeDB} {p.NameDB}", 1);
                 //result.AppendCode(tab, $"<br/>{p.TypeDB} - {p.NameDB}", 1);
                 entityList.AppendLine($"|{p.NameDB}|{p.TypeDB}|{key}|{auto}|{parent}||");
             }
@@ -155,22 +248,24 @@ namespace WorkUtilities.Domain.Services.Generator
         {
             foreach (EntryRelationship r in entry.Relationships)
             {
+                var tableName = model.EntryModels.First(x => x.Name == r.TargetName)?.NameDB;
+
                 switch (r.Type)
                 {
                     case RelationshipType.IN_1_OUT_1:
                         {
-                            result.AppendCode(tab, $"{r.TargetName} -- {entry.NameDB}", 1);                            
+                            //result.AppendCode(tab, $"{tableName} -- {entry.NameDB}", 1);                            
                         }
                         break;
                     case RelationshipType.IN_1_OUT_N:
                         {
-                            result.AppendCode(tab, $"{r.TargetName} <|-- {entry.NameDB}", 1);
-                            //result.AppendCode(tab, $"{entry.NameDB} --> {r.TargetName}", 1);
+                            //result.AppendCode(tab, $"{tableName} <|-- {entry.NameDB}", 1);
+                            result.AppendCode(tab, $"{entry.NameDB} --> {tableName}", 1);
                         }
                         break;
                     case RelationshipType.IN_N_OUT_N:
                         {
-                            result.AppendCode(tab, $"{r.TargetName} <|--|> {entry.NameDB}", 1);
+                            //result.AppendCode(tab, $"{tableName} <|--|> {entry.NameDB}", 1);
                         }
                         break;
                     default:
